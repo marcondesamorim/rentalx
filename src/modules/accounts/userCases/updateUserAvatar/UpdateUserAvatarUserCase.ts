9c5ece2e8a1aae9365dd99d21ee51335c4f7fe6e
Marcondes Amorim <marcondesjra@gmail.com>
2021-07-16 00:10:27 -0300

    Add avatar functionality

--- src/modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarUserCase.ts
+++ src/modules/accounts/userCases/updateUserAvatar/UpdateUserAvatarUserCase.ts
@@ -1,8 +1,32 @@
+import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    user_id: string;
+    avatar_file: string;
+}
+
+@injectable()
 class UpdateUserAvatarUserCase {
-    async execute() {
-        
+    constructor(
+        @inject("UserRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ user_id, avatar_file }: IRequest): Promise<void> {
+        const user = await this.usersRepository.findById(user_id);
+
+        user.avatar = avatar_file;
+
+        if (user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
+        user.avatar = avatar_file;
+
+        await this.usersRepository.create(user);
     }
 }
 
-export UpdateUserAvatarUserCase;
+export { UpdateUserAvatarUserCase };
